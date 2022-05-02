432ba4432f09bfd2ff8302642d791c1323602139
ckjohnson99 <chris-johnson99@live.com>
2022-05-01 19:43:35 -0500

    changed api call

--- js/ride.js
+++ js/ride.js
@@ -80,12 +80,11 @@ WildRydes.map = WildRydes.map || {};
 
     function handleRequestClick(event) {
         var pickupLocation = WildRydes.map.selectedPoint;
+		event.preventDefault();
+        requestUnicorn(pickupLocation);
 		getRest(pickupLocation.latitude,pickupLocation.longitude);
 		displayUpdate('Looking for something to eat? Here are some local restaurants to ask your unicorn about.');
 		displayUpdate(response.restaurants[0].name + '\n' + response.restaurants[1].name + '\n' + response.restaurants[2].name);
-		event.preventDefault();
-        requestUnicorn(pickupLocation);
-		
     }
 
     function animateArrival(callback) {
